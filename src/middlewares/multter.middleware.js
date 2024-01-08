46d0fcc1a04e56535481b0f285f73df652d98afc
ANANDMISHRAK <anandmishara370@gmail.com>
2024-01-08 14:33:10 +0530

    write code of multer Middeleware for upload photo or video in backend folder from local system

---
+++ src/middlewares/multter.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+const storage = milter.diskStorage(
+    {
+        destination: function(req, file, cb)
+        {
+            //upload in  local system to backend file 
+            cb(null, "./Public/temp")
+        },
+        filename: function(req, file, cb)
+        {
+             // file name in Backend of these photo is same is original name in local system
+            cb(null, file.originalname)
+        }
+    }
+)
+
+export const upload= multer({storage})
